8390415943ba21f125ab61495d36fb1c5de450da
fabriciobg <fabricio.gnd@gmail.com>
2018-02-20 15:39:10 -0300

    Autenticação de login
    Adicionada funcionalidade do botão ENTRAR -> função "efetuar_login"
        * Conexão com o banco de dados;
        * Comparação dos dados digitados pelo usuário com os dados salvos no banco de dados;
        * Adicionada caixa de mensagem para o caso em que o usuário fornece um login não existente.

--- telaLogin.py
+++ telaLogin.py
@@ -1,72 +1,97 @@
 from tkinter import *
+from pymysql import *
+import tkinter.messagebox
 
 
 # Funcao chamada ao clicar no botao
 def efetuar_login():
-    print('Login: ' + login.get())
-    print('Senha: ' + password.get())
-    print('O usuário deseja lembrar o login? %r' % bool(stateLembrarLogin.get()))
-    if stateLembrarLogin.get():
-        print('\tLogin salvo')
-    else:
-        print('\tLogin não salvo')
+    connection = connect(host='localhost',
+                         user='root',
+                         password='',
+                         db='cadastro',
+                         port=3307)
+    cursor = connection.cursor()
+
+    try:
+        query = 'select * from users where login = %s'
+        rows_affected = cursor.execute(query, login.get())
+        if rows_affected <= 0:
+            tkinter.messagebox.showinfo('Informação de login', 'Seu usuário ou senha está incorreto')
+        else:
+            row = cursor.fetchone()
+            if row[2] == password.get():
+                print('Tudo certo')
+            else:
+                print('Senha errada')
+
+    finally:
+        connection.close()
+
+
+#    print('Login: ' + login.get())
+#    print('Senha: ' + password.get())
+#    print('O usuário deseja lembrar o login? %r' % bool(stateLembrarLogin.get()))
+#    if stateLembrarLogin.get():
+#        print('\tLogin salvo')
+#    else:
+#        print('\tLogin não salvo')
 
 
 root = Tk()
 root.title('Protótipo v.0.1')
 
-mainFrame = Frame(root)
-mainFrame.grid(
-    column=0, row=0,
-    sticky=(N, W, E, S)
-)
+main_frame = Frame(root)
+main_frame.grid(
+                column=0, row=0,
+                sticky=(N, W, E, S))
 
-# Título na primeira linha da tela, espandido para todas as colunas
-Label(mainFrame,
+# Título na primeira linha da tela, expandido para todas as colunas
+Label(main_frame,
       text='Acesso ao sistema',
       font=('Courier', 20)
-      ).grid(row=0, column=0, columnspan=3)
+      ).grid(row=0, column=0, columnspan=3, padx=10, pady=3)
 
 # Segunda linha com label de Usuário, seguindo de campo para usuário digitar o login
-Label(mainFrame,
+Label(main_frame,
       text='Usuário:',
       font=('Courier', 12)
-      ).grid(row=1, column=0)
+      ).grid(row=1, column=0, sticky=E)
 
 login = StringVar()
-loginEntry = Entry(mainFrame,
-                   width=20,
-                   textvariable=login
-                   ).grid(row=1, column=1, columnspan=2)
+login_entry = Entry(main_frame,
+                    width=20,
+                    textvariable=login)
+login_entry.focus()
+login_entry.grid(row=1, column=1, columnspan=2, padx=10)
 
 # Segunda linha com label de senha, seguindo de campo para usuário digitar o password
-Label(mainFrame,
+Label(main_frame,
       text='Senha:',
       font=('Courier', 12)
-      ).grid(row=2, column=0)
+      ).grid(row=2, column=0, sticky=E)
 
 password = StringVar()
-passwordEntry = Entry(mainFrame,    #Password com caracteres escondidos
-                      width=20,
-                      show="*",
-                      textvariable=password
-                      ).grid(row=2, column=1, columnspan=2)
+password_entry = Entry(main_frame,  # Password com caracteres escondidos
+                       width=20,
+                       show="*",
+                       textvariable=password
+                       ).grid(row=2, column=1, columnspan=2)
 
 # Check button para o programa lembrar o usuário (nao implementado)
-stateLembrarLogin = BooleanVar()
-stateButton = Checkbutton(mainFrame,
-                          text='Lembrar usuário',
-                          offvalue=False,
-                          onvalue=True,
-                          variable=stateLembrarLogin,
-                          font=('Courier', 12),
-                          ).grid(row=3, column=0, columnspan=2)
+state_remember_login = BooleanVar()
+state_button = Checkbutton(main_frame,
+                           text='Lembrar usuário',
+                           offvalue=False,
+                           onvalue=True,
+                           variable=state_remember_login,
+                           font=('Courier', 12),
+                           ).grid(row=3, column=0, columnspan=2)
 
 # Botão para efetuar o login
-loginButton = Button(mainFrame,
-                     text='Entrar',
-                     font=('Courier', 12),
-                     command=efetuar_login
-                     ).grid(row=3, column=2)
+login_button = Button(main_frame,
+                      text='Entrar',
+                      font=('Courier', 12),
+                      command=efetuar_login
+                      ).grid(row=3, column=2)
 
 root.mainloop()
